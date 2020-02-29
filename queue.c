fc48b8db0fd0b124aaf753cd6a29ace0ac791b48
ZhuMon <richardlin0212@gmail.com>
2020-02-29 18:27:19 +0800

    Improve merge performance
    Change merge() from recursive to pseudo node

--- queue.c
+++ queue.c
@@ -208,20 +208,36 @@ void q_reverse(queue_t *q)
  */
 list_ele_t *merge(list_ele_t *l1, list_ele_t *l2)
 {
-    // merge with recursive
-    if (!l1)
-        return l2;
-    if (!l2)
-        return l1;
+    // merge with pseudo node
+    list_ele_t *temp;
 
-    int result = strcmp(l1->value, l2->value);
-    if (result < 0) {
-        l1->next = merge(l1->next, l2);
-        return l1;
+    if (strcmp(l1->value, l2->value) < 0) {
+        temp = l1;
+        l1 = l1->next;
     } else {
-        l2->next = merge(l1, l2->next);
-        return l2;
+        temp = l2;
+        l2 = l2->next;
     }
+    list_ele_t *q = temp;
+
+    while (l1 && l2) {
+        if (strcmp(l1->value, l2->value) < 0) {  // l2 > l1
+            temp->next = l1;
+            temp = temp->next;
+            l1 = l1->next;
+        } else {
+            temp->next = l2;
+            temp = temp->next;
+            l2 = l2->next;
+        }
+    }
+
+    if (l1)
+        temp->next = l1;
+    if (l2)
+        temp->next = l2;
+
+    return q;
 }
 
 list_ele_t *mergeSortList(list_ele_t *head)
@@ -250,9 +266,6 @@ list_ele_t *mergeSortList(list_ele_t *head)
 
 void q_sort(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
-
     // if q has only one element or q is empty, q->head == q->tail
     if (!q || q->head == q->tail) {
         return;
